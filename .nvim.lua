f4bf9b3073abb61af8dbb9d3fc1b0055ece8c259
Haha64142 <paul.w64142@gmail.com>
2026-05-03 00:58:29 -0500

    Add resize.cpp and make it compile

--- .nvim.lua
+++ .nvim.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>b", function()
-    vim.cmd "split | terminal cmake -S . -B build && cmake --build build && build/screenshot.exe"
+    vim.cmd "split | terminal cmake -S . -B build && cmake --build build && build/resize"
 end, { desc = "[B]uild Project" })
---
+++ resize.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+
+int main (int argc, char *argv[]) {
+    std::cout << "Hello World\n";
+
+    return 0;
+}
